e3728ccd9b83dac78a93ccf28fbfd754e0929e8b
NATALIA ESPAÑA <natacski@gmail.com>
2020-07-09 20:31:58 -0400

    weather app complete

--- weather.js
+++ weather.js
@@ -1,99 +1,68 @@
 
-let inputCity = document.getElementById('city').value.toLowerCase();
+let inputCity = document.getElementById('city');
+let tableBody = document.getElementById('table-body-content');
 const search = document.getElementById('search');
-const table = document.getElementById('info');
 const apiKey = 'd8273ff3fb899f65070a8154465cb75d'
 
 //Initial request
 search.addEventListener('click',(event) => {
-    if (input.length == 0) {
+  inputCity = document.getElementById('city').value.toLowerCase();
+  document.getElementById('weather-info').style = 'display:none';
+  document.getElementById('table-body-content').innerHTML = '';
+    if (inputCity.length == 0) {
         alert("Please insert a city name");
     }else{
-        fetch(),
-        displayInfo()
-        .then(getTableData)
-        .catch(noCity);
+        getWeatherData()
     }
 });
 
 //Fecth function
-async function fetch(url) {                                                           
-    return fetch(url)
-          .then(checkStatus) 
-          .then(res => res.json()) //get the response in json format
-          .catch(error => console.log('Looks like there was a problem', error))
-  }
-  
-  Promise.all([
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`), //fetch the city weather data
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=${apiKey}`), //fetch the data for 7 days
-    fetch('http://openweathermap.org/img/wn/10d@2x.png') // fetch the icons
-  ])
-    .then( data => {
-        const city = data[0].message;
-        const country = data[1].message;
-        const currentWeather = data[2].message;
-
-        displayInfo(city, country, currentWeather);
-
-    })
-
-    .then( data => {
-        const day = data[0].message;
-        const min = data[1].message;
-        const max = data[2].message;
-        const weather = data[3].message;
-
-        getTableData(day, min, max, weather);
-     })
-
-  //Other Functions
-
-  async function displayInfo(data) {  
-    const response = await fetch(url);
-    const dataReturn = await response.json();
-    const {} = data;  //what names should I use? city, country?
-
-  }
-
-  async function getTableData(data) {
-    const response = await fetch(url);
-    const dataReturn = await response.json();
-    const {min_temp, max_temp} = data;  // how to get data for the icons?
-    for (let i = 0; i < data.length; i++) {
-    const table_body = 
-    '<tr>' 
-        '<td>' + 'day' + data[i] + '</td>' +
-        '<td>' + 'min' + data[i] + '</td>' +
-        '<td>' + 'max' + data[i] + '</td>' +
-        '<td>' + 'weather' + data[i] + '</td>'
-     '</tr>'  
-    table.innerHTML = table_body;
+function getWeatherData() {          
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=imperial&appid=${apiKey}`)
+  .then(response => {
+    if (response.status === 200) {
+      return response.json()
+    } else {
+      alert('city not found')
+      throw 'error';
     }
+  })
+  .then( cityData => {
+          let lat = cityData.coord.lat;
+          let lon = cityData.coord.lon;
+          let city = cityData.name;
+          let country = cityData.sys.country;
+          let currentWeather = cityData.weather[0];
+          document.getElementById('current-weather').innerHTML = currentWeather.description;
+          document.getElementById('current-weather-icon').setAttribute('src', 'http://openweathermap.org/img/wn/' + currentWeather.icon + '@2x.png')
+          document.getElementById('city-name').innerHTML = city + ', ' + country;
+            
+          fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+         
+          .then( response => response.json())
+          .then( weatherData => {     
+            makeDataTable(weatherData);
+            document.getElementById('weather-info').style = 'display:block';
+            
+           }) 
+       
+                 
+        })
   }
 
-  async function noCity() {
-    if (getCityData == 0) {
-        alert('City not found!')
-    }
-}
-
-function checkStatus(response) {
-    if(response.ok) {
-      return Promise.resolve(response);
-    }else {
-    return Promise.reject(new Error(response.statusText));
-    }
+  const makeDataTable = (weatherData) => {
+    let firstDays = weatherData.daily.slice(0, 7);
+    firstDays.forEach(weatherInfo => {
+      var tr = document.createElement('tr');
+      //populate each table row with the values of the properties
+      tr.innerHTML = '<td>' + moment.unix(weatherInfo.dt).format('dddd, MMMM Do, YYYY') +  '</td>' + 
+                      '<td>' + weatherInfo.temp.min + '</td>' +
+                      '<td>' + weatherInfo.temp.max + '</td>' +
+                     '<td>' + weatherInfo.weather[0].description + '<img src=http://openweathermap.org/img/wn/' + weatherInfo.weather[0].icon + '@2x.png>' + '</td>';
+                                      
+                     
+      //add the row to the table body
+      tableBody.appendChild(tr);
+    });
   }
-
-
-
-
-
-/* Questions
-1. How to display the table data after click search button?
-2. Am I using the correct urls?
-3. How to display the weather icons? Do I need to create another function?
-4. I am trying to create async functions but I am not sure where to write await
-
-*/
+ 
